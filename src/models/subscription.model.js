87b1b603f43c292300b2358e2f5891363085b105
hellobyboy <helloworld010868@gmail.com>
2025-01-17 20:36:03 +0530

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from "mongoose"
+
+const subscriptionSchema = new mongoose.Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId,  // one who is subscribing
+        ref: "User",
+        required: true
+    },
+    channel: {
+        type: Schema.Types.ObjectId,  // one to whom 'subscriber' is subscribing
+        ref: "User",
+        required: true
+    }
+},{timestamps: true})
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
