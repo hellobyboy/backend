9ae7278421250d0196b45315383e5ef5d1705f95
hellobyboy <helloworld010868@gmail.com>
2025-01-21 19:45:30 +0530

    add getUserChannelProfile API and update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -395,6 +395,79 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Cover image updated successfully"))
 })
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+
+    const { username } = req.params
+    if(!username?.trim()){
+        throw new ApiError(400, "Username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from: "subscriptions",    //from the subscription model
+                localField: "_id",        // local field of the user
+                foreignField: "channel",   // foreign field of the subscription
+                as: "subscribers"          // store the result in subscribers, data like who has subscribed to this channel
+            }
+        },
+        {
+            $lookup:{
+                from: "subscriptions",      // from the subscription model
+                localField: "_id",      // local field of the user
+                foreignField: "subscriber",    // foreign field of the subscription
+                as: "subcribedTo"    // store the result in subscribedTo, data like to whom this channel has subscribed
+            }
+        },
+        {
+            $addFields:{
+                subscriberCount:{
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size: "$subcribedTo"
+                },
+                isSubscribed:{    // check if the logged in user is subscribed to this channel or not // the logic is to check if the logged in user is in the list of subscribers or not
+                    $cond:{
+                        $if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName: 1,
+                username: 1,
+                subscriberCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+    console.log(channel);
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel does not exists")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "Channel profile fetched successfully"))
+    
+    
+    
+})
+
 export {
     registerUser,
     loginUser,
@@ -404,5 +477,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }
